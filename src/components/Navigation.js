61d7df297c1f449320a9e2ebc29790a22c784aa1
Tera-Si <cheuklunterence@gmail.com>
2021-08-08 03:58:30 +0800

    added Home and About button to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,31 @@
 import React from "react"
 import { useSelector } from "react-redux"
-import { Navbar } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 
 const Navigation = () => {
   const auth = useSelector(state => state.auth)
+
+  const navLinkStyle = {
+    marginLeft: 15
+  }
 
   return (
     <Navbar collapseOnSelect bg="primary" variant="dark" expand="lg">
       <Navbar.Brand style={{ marginLeft: 15}}>uniFree</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-      {!auth
-        ? null
-        : <p>Logged in</p>
-      }
+        <Nav className="me-auto">
+          <Nav.Link as="span" style={navLinkStyle}>
+            Home
+          </Nav.Link>
+          <Nav.Link as="span" style={navLinkStyle}>
+            About
+          </Nav.Link>
+        </Nav>
+        {!auth
+          ? null
+          : <p>Logged in</p>
+        }
       </Navbar.Collapse>
     </Navbar>
   )
