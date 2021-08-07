c274d9456c3d38043b549233db74df6e81e235e5
Tera-Si <cheuklunterence@gmail.com>
2021-08-08 04:00:30 +0800

    changed label column width

--- src/components/LabelledInputRow.js
+++ src/components/LabelledInputRow.js
@@ -4,8 +4,8 @@ import { Form, Row, Col } from "react-bootstrap"
 const LabelledInputRow = forwardRef((props, ref) => {
   return (
     <Form.Group as={Row}>
-      <Form.Label column sm="2">{props.label}</Form.Label>
-        <Col sm="10">
+      <Form.Label column sm="3">{props.label}</Form.Label>
+        <Col sm="9">
           <Form.Control type={props.type} placeholder={props.placeholder} ref={ref} />
         </Col>
     </Form.Group>
