44c843899421302cf45d95692b75c61cd76e0560
Tera-Si <cheuklunterence@gmail.com>
2021-08-16 10:59:39 +0800

    implemented hover background-color for form.control input fields

--- src/components/LabelledInputRow.js
+++ src/components/LabelledInputRow.js
@@ -1,12 +1,13 @@
 import React, { forwardRef } from "react"
 import { Form, Row, Col } from "react-bootstrap"
+import "../styles/LabelledInputRow.css"
 
 const LabelledInputRow = forwardRef((props, ref) => {
   return (
     <Form.Group as={Row}>
       <Form.Label column sm="3">{props.label}</Form.Label>
         <Col sm="9">
-          <Form.Control type={props.type} placeholder={props.placeholder} disabled={props.disabled} ref={ref} />
+          <Form.Control type={props.type} placeholder={props.placeholder} disabled={props.disabled} ref={ref} className="inputField" />
         </Col>
     </Form.Group>
   )
