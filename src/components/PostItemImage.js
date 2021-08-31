3671e895e48ec2b269fac3c298909bf798c475a5
Tera-Si <cheuklunterence@gmail.com>
2021-09-01 00:10:58 +0800

    implemented client side checking for image upload

--- src/components/PostItemImage.js
+++ src/components/PostItemImage.js
@@ -1,26 +1,31 @@
 import React, { createRef } from "react"
+import { useDispatch } from "react-redux"
 import { Button, Carousel, Form, Image, Row, Col } from "react-bootstrap"
 import CardWrapper from "./CardWrapper"
+import { actionSetErrorNotice } from "../reducers/notificationReducer"
 import "../styles/PostItem.css"
 
 const PostItemImage = ({ uploadedImages, setUploadedImages, loading }) => {
   const uploadField = createRef()
+  const dispatch = useDispatch()
 
   const handleUploadImage = (event) => {
     event.preventDefault()
     const currentFiles = Array.from(uploadField.current.files)
+
+    if (uploadedImages.length >= 8) {
+      uploadField.current.value = null
+      dispatch(actionSetErrorNotice("Error: maximum image number exceeded"))
+      return
+    }
 
     for (let file of currentFiles) {
       const isValidImage = file.name.endsWith(".jpg") || file.name.endsWith(".jpeg")
                            file.name.endsWith(".png") || file.name.endsWith(".gif")
 
       if (!isValidImage) {
-        // TODO: throw error / notification
         uploadField.current.value = null
-
-        // temporary
-        console.log("invalid image!")
-
+        dispatch(actionSetErrorNotice("Error: invalid file types selected"))
         return
       }
     }
