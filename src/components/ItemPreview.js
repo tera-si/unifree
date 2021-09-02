cced31af816e646b7030e07c860849cd4d2adaa8
Tera-Si <cheuklunterence@gmail.com>
2021-09-02 18:28:06 +0800

    added styles

--- src/components/ItemPreview.js
+++ src/components/ItemPreview.js
@@ -1,24 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link, Redirect } from "react-router-dom"
 import { Card } from "react-bootstrap"
+import "../styles/ItemPreview.css"
 
 const ItemPreview = (
-  { firstImage, name, category, condition, datePosted, username }
+  { id, firstImage, name, category, condition, datePosted, username }
 ) => {
+  const [redirect, setRedirect] = useState(false)
+
   const imgSrc = `/uploads/items/images/${firstImage}`
   const parsedDate = new Date(datePosted).toLocaleDateString()
 
+  const handleImgClick = () => {
+    setRedirect(true)
+  }
+
+  if (redirect) {
+    return <Redirect to={`/view_item/${id}`} />
+  }
+
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card className="cardBase">
       <Card.Header>{parsedDate}</Card.Header>
-      <Card.Img variant="top" src={imgSrc} />
+      <Card.Img variant="top" src={imgSrc} className="cardImage" onClick={handleImgClick} />
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Subtitle>{category}</Card.Subtitle>
-        <Card.Text>
-          {condition}
-        </Card.Text>
+        <Card.Title>
+          <Link to={`/view_item/${id}`} className="cardTitle">{name}</Link>
+        </Card.Title>
+        <Card.Subtitle className="cardText">{category}</Card.Subtitle>
+        <Card.Text className="cardText">{condition}</Card.Text>
       </Card.Body>
-      <Card.Footer> {username} </Card.Footer>
+      <Card.Footer>{username}</Card.Footer>
     </Card>
   )
 }
