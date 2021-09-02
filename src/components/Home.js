06254c31f91a407f4361ec065fcfe9fd8d78eb2c
Tera-Si <cheuklunterence@gmail.com>
2021-09-02 17:21:04 +0800

    items are now sorted by latest date

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   useEffect(() => {
     const getAllItems = async () => {
       const data = await itemService.getAll()
+      data.sort((item1, item2) => -(new Date(item1.datePosted) - new Date(item2.datePosted)))
       setItems(data)
     }
 
