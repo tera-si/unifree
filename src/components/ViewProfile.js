196417313b9ab653610d9dc4319a95f3022d8896
Tera-Si <cheuklunterence@gmail.com>
2021-09-03 15:09:51 +0800

    implemented view_profile page

--- src/components/ViewProfile.js
+++ src/components/ViewProfile.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { Card, CardGroup } from "react-bootstrap"
 import CardWrapper from "./CardWrapper"
 import ProfileHeader from "./ProfileHeader"
+import ItemPreview from "./ItemPreview"
 import userService from "../services/userService"
 
 const ViewProfile = () => {
@@ -23,9 +25,30 @@ const ViewProfile = () => {
     )
   }
 
+  const itemSorted = profile.items.sort((item1, item2) => -(new Date(item1.datePosted) - new Date(item2.datePosted)))
+
   return (
     <CardWrapper>
       <ProfileHeader username={profile.username} id={profile.id} />
+      <CardWrapper>
+        <Card.Title>Latest items from {profile.username}</Card.Title>
+        <CardGroup>
+          {itemSorted.map(item =>
+            <div key={item.id}>
+              <ItemPreview
+                id={item.id}
+                firstImage={item.imagePaths[0]}
+                name={item.name}
+                category={item.category}
+                condition={item.condition}
+                datePosted={item.datePosted}
+                username={profile.username}
+                userID={profile.id}
+              />
+            </div>
+          )}
+        </CardGroup>
+      </CardWrapper>
     </CardWrapper>
   )
 }
