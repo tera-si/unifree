c1a485455037695b850392814e650635b8eb695c
Tera-Si <cheuklunterence@gmail.com>
2021-09-11 23:51:17 +0800

    shortened long line

--- src/components/LabelledInputRow.js
+++ src/components/LabelledInputRow.js
@@ -7,7 +7,13 @@ const LabelledInputRow = forwardRef((props, ref) => {
     <Form.Group as={Row}>
       <Form.Label column sm="3">{props.label}</Form.Label>
         <Col sm="9">
-          <Form.Control type={props.type} placeholder={props.placeholder} disabled={props.disabled} ref={ref} className="inputField" />
+          <Form.Control
+            type={props.type}
+            placeholder={props.placeholder}
+            disabled={props.disabled}
+            ref={ref}
+            className="inputField"
+          />
         </Col>
     </Form.Group>
   )
