48396787a51e1c9e2075fa46842f0538b29c6357
tera-si <cheuklunterence@gmail.com>
2021-10-18 13:09:36 +0800

    disabled socket auto reconnection to prevent duplicate error messages

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,9 @@ import { io } from "socket.io-client"
 
 //! change this in production
 const URL = `${process.env.REACT_APP_BACKEND_ADDRESS}:5000`
-const socket = io(URL, { autoConnect: false })
+const socket = io(URL, {
+  autoConnect: false,
+  reconnection: false
+})
 
 export default socket
